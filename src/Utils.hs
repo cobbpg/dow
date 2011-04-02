ae0ea2ec93bf87d1c194aee3a2cf66542a760064
Patai Gergely <patai.gergely@gmail.com>
2011-04-02 21:52:30 +0200

    Updated for 2.2.

--- src/Utils.hs
+++ src/Utils.hs
@@ -11,12 +11,6 @@ import FRP.Elerea.Simple
 
 infix 2 -->
 
-transfer2 x0 f s1 s2 = mfix $ \sig -> liftA3 f s1 s2 <$> delay x0 sig
-
-transfer3 x0 f s1 s2 s3 = mfix $ \sig -> liftM4 f s1 s2 s3 <$> delay x0 sig
-
-transfer4 x0 f s1 s2 s3 s4 = mfix $ \sig -> liftM5 f s1 s2 s3 s4 <$> delay x0 sig
-
 edge s = do
   s' <- delay False s
   return $ s' >>= \x -> if x then return False else s
